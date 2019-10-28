01ea45d3ed9fde83c03ef1c08ab66ba6eb42f7d5
HiroakiMorita <taiyou3291@outlook.jp>
2019-10-29 02:59:49 +0900

    chat機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         has_many :chat_messages
+         has_many :chat_room_users
+
          mount_uploader :img_name, ImgNameUploader
          enum sex: { 男: 0, 女: 1 }
+         validates :img_name, presence: true
 end
